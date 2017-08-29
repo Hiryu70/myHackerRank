dcb457bca723a696c22758f080fb5906d7c5590d
Hiryu70 <che.maxv@gmail.com>
2017-08-29 19:53:41 +0700

    Create 2D Array - DS.cs

---
+++ 2D Array - DS.cs
@@ -0,0 +1,32 @@
+// https://www.hackerrank.com/challenges/2d-array
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) 
+    {
+        int[][] arr = new int[6][];
+        for(int arr_i = 0; arr_i < 6; arr_i++)
+        {
+           string[] arr_temp = Console.ReadLine().Split(' ');
+           arr[arr_i] = Array.ConvertAll(arr_temp,Int32.Parse);
+        }
+        
+        int maxSum = -50;
+        for (int i = 1; i < 5; i++)
+        {
+            for (int j = 1; j < 5; j++)
+            {
+                int curSum = 0;
+                curSum = curSum + arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1];
+                curSum = curSum +               + arr[i+0][j];
+                curSum = curSum + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1];
+                maxSum = (curSum > maxSum) ? curSum : maxSum ;
+            }
+        }
+        Console.WriteLine(maxSum);
+    }
+}
