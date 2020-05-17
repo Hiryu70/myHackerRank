cb85982f094af4ee5dc40ed55660687d7f022ccd
Hiryu70 <che.maxv@gmail.com>
2020-05-17 17:18:12 +0300

    Create rotatingArrays.cs

---
+++ rotatingArrays.cs
@@ -0,0 +1,47 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        var firstArray = a.Take(d).ToArray();
+        var secondArray = a.Skip(d).ToArray();
+
+        int[] newArray = new int[firstArray.Length + secondArray.Length];
+        Array.Copy(secondArray, newArray, secondArray.Length);
+        Array.Copy(firstArray, 0, newArray, secondArray.Length, firstArray.Length);
+
+        return newArray;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nd = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nd[0]);
+
+        int d = Convert.ToInt32(nd[1]);
+
+        int[] a = Array.ConvertAll(Console.ReadLine().Split(' '), aTemp => Convert.ToInt32(aTemp))
+        ;
+        int[] result = rotLeft(a, d);
+
+        textWriter.WriteLine(string.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
